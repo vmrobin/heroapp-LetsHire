a89fd1568d51a474a661360745249d7d53c297e8
Henry Chen <hchen@vmware.com>
2013-03-19 10:24:43 +0800

    add built-in Model 'Department'
    Signed-off-by: Henry Chen <hchen@vmware.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130319022125) do
+
+  create_table "departments", :force => true do |t|
+    t.string "name"
+    t.string "description"
+  end
+
+  add_index "departments", ["name"], :name => "index_departments_on_name", :unique => true
 
 end
