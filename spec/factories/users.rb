3d02786b82a56ecbc1a619611c86b0f096563a4b
Yisui Hu <yisuih@vmware.com>
2013-03-19 17:48:17 +0800

    use devise for user login logic

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :user do
     email Faker::Internet.email
     name Faker::Name.name
-    password Digest::SHA1.base64digest((Random.rand * 10000).to_i.to_s)
-    admin Random.rand >= 0.5
+    password (Random.rand * 10000).to_i.to_s
   end
 end
