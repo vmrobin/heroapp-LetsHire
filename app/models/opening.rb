a38c7ac9b4d30f0672af0125209a50daa5821dfd
Henry Chen <hchen@vmware.com>
2013-03-22 12:01:48 +0800

    [46355869] JobDescription: add 'city' field
    Signed-off-by: Henry Chen <hchen@vmware.com>

--- app/models/opening.rb
+++ app/models/opening.rb
@@ -3,7 +3,7 @@ require 'carmen'
 class Opening < ActiveRecord::Base
   include Carmen
 
-  attr_accessible :title, :country, :province, :description, :department_id, :status, :hiring_manager_id, :recruiter_id
+  attr_accessible :title, :country, :province, :city, :description, :department_id, :status, :hiring_manager_id, :recruiter_id
   belongs_to :department
   belongs_to :hiring_manager, :class_name => "User", :foreign_key => :hiring_manager_id, :readonly => true
   belongs_to :recruiter, :class_name => "User", :foreign_key => :recruiter_id, :readonly => true
@@ -16,19 +16,22 @@ class Opening < ActiveRecord::Base
   end
 
   def full_address
-    unless country.nil?
+    if country.nil?
+      (city || "UNKNOWN")
+    else
       Country.coded(country).try { |country_obj|
         sub_regions = country_obj.subregions
         province_obj = sub_regions.respond_to?(:coded) ?  sub_regions.coded(province) : nil
         logger.debug sub_regions.inspect
         logger.debug province
         if province_obj.nil?
-          (province || "UNKNOWN") + ', ' + country_obj.name
+          (city || "UNKNOWN") + ',' + (province || "UNKNOWN") + ', ' + country_obj.name
         else
-          province_obj.name.to_s + ',' + country_obj.name
+          (city || "UNKNOWN") + ',' + province_obj.name.to_s + ',' + country_obj.name
         end
       }
     end
+
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130321083307) do
+ActiveRecord::Schema.define(:version => 20130322035010) do
 
   create_table "departments", :force => true do |t|
     t.string "name",        :null => false
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20130321083307) do
     t.integer  "status",            :default => 0
     t.datetime "created_at",                       :null => false
     t.datetime "updated_at",                       :null => false
+    t.string   "city"
   end
 
   create_table "users", :force => true do |t|
