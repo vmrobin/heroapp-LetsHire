c13cd6d7c6ec44e71005dd6bf5614d92a7b23b8e
Henry Chen <hchen@vmware.com>
2013-04-15 17:31:13 +0800

    [48062455] fix bug when interviewer cannot selected
    Signed-off-by: Henry Chen <hchen@vmware.com>

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -3,5 +3,7 @@ class Assessment < ActiveRecord::Base
 
   belongs_to :opening_candidate
 
+  validates :comment, :length => { :minimum => 1 }
+
   belongs_to :creator, :class_name => "User", :foreign_key => :creator_id, :readonly => true
 end
