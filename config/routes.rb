f681d870385efc33d917d0c68a679e5f4c53118f
Chunjie <czhu@vmware.com>
2013-04-16 19:33:07 -0700

    [PT 48051223] remove /about page

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ LetsHire::Application.routes.draw do
 
   root to: 'static_pages#home'
   match '/help', to: 'static_pages#help'
-  match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 
   match '/profile', to: 'profile#edit'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -20,13 +20,6 @@ describe "Static pages" do
 
   end
 
-  describe "About page" do
-    before { visit about_path }
-
-    it { should have_selector('h1',    text: 'About') }
-    it { should have_selector('title', text: full_title('About Us')) }
-  end
-
   describe "Contact page" do
     before { visit contact_path }
 
