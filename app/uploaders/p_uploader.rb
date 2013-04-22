9e748565274a7f20772e5476464576eb4278d067
Yuan Zhuang <yuanzhuang@vmware.com>
2013-04-22 11:55:08 +0800

    [48243769] add callbacks to delete the tmp files

--- app/uploaders/p_uploader.rb
+++ app/uploaders/p_uploader.rb
@@ -14,10 +14,24 @@ class PUploader < CarrierWave::Uploader::Base
   storage :postgresql_lo
   # storage :fog
 
+  before :store, :remember_cache_id
+  after :store, :delete_tmp_dir
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def remember_cache_id(new_file)
+    @cache_id_was = cache_id
+  end
+
+  def delete_tmp_dir(new_file)
+    # make sure we don't delete other things accidentally by checking the name pattern
+    if @cache_id_was.present? && @cache_id_was =~ /\A[\d]{8}\-[\d]{4}\-[\d]+\-[\d]{4}\z/
+      FileUtils.rm_rf(File.join(root, cache_dir, @cache_id_was))
+    end
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
