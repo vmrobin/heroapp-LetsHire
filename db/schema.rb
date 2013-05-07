8ab63fbbd529e200ea98250a69f45c7c2b87fbb1
Henry Chen <hchen@vmware.com>
2013-05-07 09:34:18 +0800

    refresh db/schema.db based on latest definition
    Signed-off-by: Henry Chen <hchen@vmware.com>

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(:version => 20130428012939) do
     t.string   "phone"
     t.string   "source"
     t.text     "description"
-    t.string   "resume"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
